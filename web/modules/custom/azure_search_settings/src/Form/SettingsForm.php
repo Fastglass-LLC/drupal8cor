d7f9ab7d76f3b4f19250168fecc5882a4f870166
Scott Weiss <scott@fastglass.net>
2019-02-24 19:49:37 -0600

    COR-28 - Adding the api-key setting value.  Also added code to delete/add the Azure Search Indexes/Fields criteria as defined in Azure Search endpoint.

--- web/modules/custom/azure_search_settings/src/Form/SettingsForm.php
+++ web/modules/custom/azure_search_settings/src/Form/SettingsForm.php
@@ -4,6 +4,10 @@ namespace Drupal\azure_search_settings\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use \Drupal\node\Entity\Node;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 
 /**
  * Configure Azure Search Settings settings for this site.
@@ -61,6 +65,15 @@ class SettingsForm extends ConfigFormBase {
       '#description' => t('This is the api version of endpoint of your Azure Search service.  The default supported value at this time is "2017-11-11"'),
     ];
 
+    $form['azure_search_settings']['api-key'] = [
+      '#type' => 'textfield',
+      '#title' => t('API Key'),
+      '#default_value' => $this->config('azure_search_settings.settings')
+        ->get('api-key'),
+      '#size' => 40,
+      '#description' => t('This is the api key of endpoint of your Azure Search service.'),
+    ];
+
     $form['azure_search_indexes']['resync_indexes'] = [
       '#type' => 'submit',
       '#value' => 'Resync Indexes',
@@ -76,8 +89,82 @@ class SettingsForm extends ConfigFormBase {
    */
   public function resyncIndexes(array &$form, FormStateInterface $form_state) {
 
-    $this->messenger()->addStatus($this->t('I got clicked!!!'));
+    $config = \Drupal::config('azure_search_settings.settings');
 
+    $x = 0;
+    $y = 0;
+
+    //Call Azure to get the Search Indexes and related details.
+    $client = new Client();
+    $request = new Request(
+      "GET",
+      "https://" . $config->get('endpoint') . ".search.windows.net/indexes?api-version=" . $config->get('api-version'),
+      [
+        "api-key" => $config->get('api-key'),
+        "content-type" => "application/json",
+      ],
+      "{}");
+
+    $response = $client->send($request);
+    $azure_search_indexes = json_decode($response->getBody());
+
+    //TODO - Need to add proper error logging.
+    if ($response->getStatusCode() != 200) {
+      $this->messenger()
+        ->addError('There was an error calling the web service \n' . $response->getBody());
+    }
+
+    //TODO - Need to refactor out the entity_delete_multiple function calls as it is deprecated in D9.
+    //Delete all the existing content if any exists.
+    $existing_fields = \Drupal::entityQuery('node')
+      ->condition('type', 'azure_search_index_field')
+      ->execute();
+    entity_delete_multiple('node', $existing_fields);
+
+    $existing_indexes = \Drupal::entityQuery('node')
+      ->condition('type', 'azure_search_index')
+      ->execute();
+    entity_delete_multiple('node', $existing_indexes);
+
+    $existing_fields = NULL;
+    $existing_indexes = NULL;
+
+    //Iterate through the web service response for Indexes.
+    foreach ($azure_search_indexes->value as $index) {
+
+      //Add the Search Index to Drupal.
+      $index_node = Node::create([
+        'type' => 'azure_search_index',
+        'title' => $index->name,
+        'path' => ['alias' => '/index/' . $index->name],
+      ]);
+      $index_node->save();
+      $x++;
+
+      //Iterate through the web service response for Index Fields.
+      foreach ($index->fields as $field) {
+
+        //Add the Search Index Field to Drupal.
+        $field_node = Node::create([
+          'type' => 'azure_search_index_field',
+          'title' => $field->name,
+          'field_azure_search_index_field' => $index_node->id(),
+          'field_azure_search_field_facet' => $field->facetable,
+          'field_azure_search_field_filter' => $field->filterable,
+          'field_azure_search_field_key' => $field->key,
+          'field_azure_search_field_retriev' => $field->retrievable,
+          'field_azure_search_field_sortabl' => $field->sortable,
+          'field_azure_search_index_fld_bln' => $field->searchable,
+          'field_azure_search_index_fld_txt' => $field->type,
+          'path' => ['alias' => '/index/' . $index->name . '/' . $field->name],
+        ]);
+        $field_node->save();
+        $y++;
+      }
+    }
+
+    $this->messenger()
+      ->addStatus($this->t('Added  ' . $x . ' Index node(s) and ' . $y . ' Field node(s) as defined in the Azure Search endpoint.'));
   }
 
   /**
@@ -93,6 +180,10 @@ class SettingsForm extends ConfigFormBase {
       $form_state->setErrorByName('api-version', $this->t('The value for API Version cannot be empty.'));
     }
 
+    if (strlen($form_state->getValue('api-key')) == 0) {
+      $form_state->setErrorByName('api-key', $this->t('The value for API Key cannot be empty.'));
+    }
+
     parent::validateForm($form, $form_state);
   }
 
@@ -101,9 +192,9 @@ class SettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('azure_search_settings.settings')
-      //->set('example', $form_state->getValue('example'))
       ->set('endpoint', $form_state->getValue('endpoint'))
       ->set('api-version', $form_state->getValue('api-version'))
+      ->set('api-key', $form_state->getValue('api-key'))
       ->save();
     parent::submitForm($form, $form_state);
   }
